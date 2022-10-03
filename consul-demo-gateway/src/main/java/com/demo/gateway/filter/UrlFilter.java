83f4f5b242be95a7cc52cdab60ede11ed4745a58
Fangang <fangang2004@126.com>
2022-10-03 16:51:53 +0800

    modify the gateway filters

--- consul-demo-gateway/src/main/java/com/demo/gateway/filter/UrlFilter.java
+++ consul-demo-gateway/src/main/java/com/demo/gateway/filter/UrlFilter.java
@@ -8,18 +8,23 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
-
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+
 /**
+ * The filter that get the user's method and url
  * @author fangang
  */
 @Component
-public class LogFilter implements GlobalFilter, Ordered {
-	private static final Log log = LogFactory.getLog(LogFilter.class);
+public class UrlFilter implements GlobalFilter, Ordered {
+	private static final Log log = LogFactory.getLog(UrlFilter.class);
+	private static final String FORBIDDEN_URL = "/foo";
 	@Override
 	public int getOrder() {
 		return 1;
@@ -28,10 +33,21 @@ public class LogFilter implements GlobalFilter, Ordered {
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 		ServerHttpRequest request = exchange.getRequest();
-	     log.info(
+		log.info(
 	                String.format("send %s[method] request to %s[url]",
 	                		request.getMethod(),
 	                		request.getURI()));
+		if(isForbiddenUrl(request.getURI())) return forbidden(exchange);
 		return chain.filter(exchange);
+	}
+
+	private Mono<Void> forbidden(ServerWebExchange exchange) {
+		ServerHttpResponse response = exchange.getResponse();
+		response.setStatusCode(HttpStatus.FORBIDDEN);
+		return response.setComplete();
+	}
+
+	private boolean isForbiddenUrl(URI url) {
+		return FORBIDDEN_URL.equals(url.toString());
 	}
 }
